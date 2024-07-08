f56dc2a980dd3641b13acbe55c63eaee2c767345
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-07-08 20:38:54 -0300

    1st commit

---
+++ resources/views/content.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <h1>Posts</h1>
+
+    <a href="/post/post1">Here the link for post 1</a><br>
+    <a href="/post/post2">Here the link for post 2</a>
+</x-layout>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <h1>Post Title</h1>
+    
+    <p>Post content goes here</p>
+</x-layout>
