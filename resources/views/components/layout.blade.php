e3b34d366531798a4765782722b7bc805e314c7e
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-07-23 19:36:04 -0300

    css linked and working

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}" >
 </head>
 <body>
     <nav class="bg-white">
--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -5,7 +5,8 @@
 
     <h1>Posts</h1>
 
-    <div class="parsedown">
+    <div class="parsedown p-4 mx-auto flex flex-col gap-y-4 sm:w-auto md:w-1/2">
+
         {!! $htmlText !!}
     </div>
 </x-layout>
