7bb3c6ba07833f26c41dcbd41888082a6b96bc17
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-07-23 21:08:58 -0300

    lists styled

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -3,10 +3,7 @@
         Content
     </x-slot:title>
 
-    <h1>Posts</h1>
-
     <div class="parsedown p-4 mx-auto flex flex-col gap-y-2 sm:w-auto md:w-1/2">
-
         {!! $htmlText !!}
     </div>
 </x-layout>
