b38fa2ec8b788676e9146be017b5e92296da2220
bernardoazevedo <bernardoazevedo.8@gmail.com>
2024-07-25 21:06:04 -0300

    text color adjust

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -3,7 +3,7 @@
         Content
     </x-slot:title>
 
-    <div class="parsedown px-4 mb-8 mx-auto flex flex-col gap-y-2 sm:w-auto md:w-1/2">
+    <div class="parsedown px-4 mb-8 mx-auto flex flex-col gap-y-2 sm:w-auto md:w-1/2 text-gray-600">
         {!! $htmlText !!}
     </div>
 </x-layout>
