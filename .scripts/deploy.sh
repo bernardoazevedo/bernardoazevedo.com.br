02d847b74c2683e181473ddb23ec6f50ba46eba5
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2024-12-28 14:55:23 -0300

    addind deploy script

---
+++ .scripts/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -u
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+
+# Hostinger setup
+wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install 16
+
+alias composer="php /usr/local/bin/composer2.phar"
+
+source ~/.bashrc
+# Hostinger setup
+
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+npm run prod
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
