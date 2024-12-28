443822660d78dd5088304177119a05fa0c0241bb
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2024-12-28 15:57:13 -0300

    imprimindo erro se não gerar .env

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -14,7 +14,7 @@ nvm install 16
 source ~/.bashrc
 
 echo "Copiando .env..."
-cp .env.example .env
+cp .env.example .env || echo "Erro ao gerar .env"
 
 echo "Baixando Composer..."
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
