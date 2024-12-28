c2efe9f936b1efd20bacd830adb7fe4303b4fd4b
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2024-12-28 15:06:53 -0300

    deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,28 +4,12 @@ set -u
 
 echo "Deployment started ..."
 
+
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
 
-
-# Hostinger setup
-wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-nvm install 16
-
-alias composer="php /usr/local/bin/composer2.phar"
-
-source ~/.bashrc
-# Hostinger setup
-
-
-# Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+cp .env.example .env
 
 # Clear the old cache
 php artisan clear-compiled
@@ -33,11 +17,14 @@ php artisan clear-compiled
 # Recreate cache
 php artisan optimize
 
-# Compile npm assets
-npm run build
-
 # Run database migrations
 php artisan migrate --force
+
+php artisan key:generate --ansi
+
+php artisan storage:link
+
+ln -s public public_html
 
 # Exit maintenance mode
 php artisan up
