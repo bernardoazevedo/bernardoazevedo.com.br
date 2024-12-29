c33d4cf518b45e6f75aa87838769474f0b2d3e00
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2024-12-28 22:25:09 -0300

    editing text

--- resources/views/content/partials/parsed-content.blade.php
+++ resources/views/content/partials/parsed-content.blade.php
@@ -1,12 +1,8 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Parsed Content') }}
+            {{ __('Your content will be displayed like this:') }}
         </h2>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __("Your content will be displayed like this:") }}
-        </p>
     </header>
 
     <div id="htmlText" class="parsedown flex flex-col gap-y-2 sm:w-auto text-gray-600">
