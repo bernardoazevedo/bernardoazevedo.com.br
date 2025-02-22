541987331eeb4e96ee293cf29097e1ecb3db11c5
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-02-22 10:59:29 -0300

    exibindo mensagem quando não há conteúdo cadastrado

--- resources/views/content/list-content.blade.php
+++ resources/views/content/list-content.blade.php
@@ -6,7 +6,7 @@
     <div class="list-content px-2 mb-8 mx-auto flex flex-col gap-y-1 sm:w-auto md:w-1/2 text-gray-600">
         <h1>{{ __('Content') }}</h1>
         
-        @if($contents)
+        @if($contents && count($contents))
             <ul class="text-lg font-semibold list-disc">
                 @foreach($contents as $content)
                     <li class="mt-3 ">
@@ -17,7 +17,7 @@
             </ul>
 
         @else 
-            <h2>Ops... Conteúdo não encontrado</h2>
+            <h2>Ops... Ainda não há nenhum conteúdo</h2>
         @endif
     </div>
 </x-layout>
