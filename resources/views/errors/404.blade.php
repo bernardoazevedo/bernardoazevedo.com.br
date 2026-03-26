6c5a718c449990588bc53bd399653eaf066563f7
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2026-03-25 23:05:08 -0300

    implementando estilos do system.css

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,15 +1,19 @@
 <x-layout>
     <x-slot:title>
-        {{ __('Content') }}
+        {{ __('Erro 404') }}
     </x-slot:title>
 
-    <div class="list-content px-2 mb-8 mx-auto flex flex-col gap-y-1 sm:w-auto md:w-1/2 text-gray-600">
-        <h1>{{ __('Erro 404') }}</h1>
-        
+    <div class="standard-dialog center error-dialog">
+        <h1 class="dialog-text">{{ __('Erro 404') }}</h1>
+
         @if (config('app.debug'))
-            <p>{{ $exception->getMessage() }}</p>
+            <p class="dialog-text">{{ $exception->getMessage() }}</p>
         @else
-            <p>{{ __('Ops... O conteúdo não foi encontrado, tente novamente.') }}</p>
+            <p class="dialog-text">{{ __('Ops... O conteúdo não foi encontrado, tente novamente.') }}</p>
         @endif
+
+        <div class="flex justify-center mt-4">
+            <a href="/" class="btn btn-default">Home</a>
+        </div>
     </div>
 </x-layout>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,15 +1,19 @@
 <x-layout>
-	<x-slot:title>
-		{{ __('Content') }}
-	</x-slot:title>
+    <x-slot:title>
+        {{ __('Erro 500') }}
+    </x-slot:title>
 
-	<div class="list-content px-2 mb-8 mx-auto flex flex-col gap-y-1 sm:w-auto md:w-1/2 text-gray-600">
-		<h1>{{ __('Erro 500') }}</h1>
-		
-		@if (config('app.debug'))
-			<p>{{ $exception->getMessage() }}</p>
-		@else
-			<p>{{ __('Ops... Ocorreu um erro interno no servidor, tente novamente.') }}</p>
-		@endif
-	</div>
+    <div class="standard-dialog center error-dialog">
+        <h1 class="dialog-text">{{ __('Erro 500') }}</h1>
+
+        @if (config('app.debug'))
+            <p class="dialog-text">{{ $exception->getMessage() }}</p>
+        @else
+            <p class="dialog-text">{{ __('Ops... Ocorreu um erro interno no servidor, tente novamente.') }}</p>
+        @endif
+
+        <div class="flex justify-center mt-4">
+            <a href="/" class="btn btn-default">Home</a>
+        </div>
+    </div>
 </x-layout>
